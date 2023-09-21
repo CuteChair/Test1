1014aab1e3d182ac9c3ab565ddbd3a41cdd417ce
Chair <jules.lhp@gmail.com>
2023-09-21 11:50:06 -0400

    temperature

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+Console.Write("Bonjour, quelle est votre nom : ");
+string n = Console.ReadLine();
+
+//string interpolation
+Console.WriteLine($"Bienvenue {n} dans l<application!");
+Console.Write("entrer une temperature : ");
+
+double celc = Convert.ToDouble(Console.ReadLine());
+double far = celc * 9 / 5 + 32;
+
+Console.WriteLine($"Il fait {far} degres fahrenheit: ");
+Console.ReadKey();
